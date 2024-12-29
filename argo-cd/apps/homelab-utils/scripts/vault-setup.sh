e7c313c0353ee810f6e63e7c0c849f41c67d239f
this-is-tobi <this-is-tobi@proton.me>
2024-12-29 23:58:46 +0100

    refactor: improve install tests

--- argo-cd/apps/homelab-utils/scripts/vault-setup.sh
+++ argo-cd/apps/homelab-utils/scripts/vault-setup.sh
@@ -7,7 +7,21 @@ generate_password() {
 
 # Jq commands
 merge_json_jq='
-  ($current // {}) + ($data // {})
+  def merge_conditionally(a; b):
+    reduce (a | keys_unsorted[]) as $key
+    (b; if b[$key] == null or b[$key] == "" then .[$key] = a[$key] else . end);
+  def recursive_merge(a; b):
+    reduce (a | keys_unsorted[]) as $key
+    (b;
+      if (a[$key] | type) == "object" and (b[$key] | type) == "object" then
+        .[$key] = recursive_merge(a[$key]; b[$key])
+      elif b[$key] == null or b[$key] == "" then
+        .[$key] = a[$key]
+      else
+        .
+      end
+    );
+  recursive_merge($data; $current)
 '
 
 get_password_path='
@@ -47,8 +61,6 @@ for SECRET in $(echo "$SECRETS" | base64 -d | jq -c ".[]"); do
   # Prepare JSON payload
   JSON_PAYLOAD=$(printf '{"data":%s}' "$UPDATED_DATA")
 
-  echo "CURRENT_VAULT_DATA: $CURRENT_VAULT_DATA"
-  echo "UPDATED_DATA: $UPDATED_DATA"
   # Compare the current Vault data with the updated data
   if [[ "$CURRENT_VAULT_DATA" != "$UPDATED_DATA" ]]; then
     echo "Updating secret at $VAULT_PATH in Vault..."
